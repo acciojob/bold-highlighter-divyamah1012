4b6462c2f6df3d5368cca84c04eba0b063687d15
divyamah1012 <121602719+divyamah1012@users.noreply.github.com>
2025-05-27 12:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function highlight() {
     //Write your code here
+let query=document.querySelectorAll('strong');
 
+	for(let q of query){
+		q.style.color =  "rgb(0, 128, 0)";
+	}
+	
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+let query1=document.querySelectorAll('strong');
+	for(let q of query1){
+		q.style.color="black";
+	}
     
 }
